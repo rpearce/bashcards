0b30c1a8c276dd78b02e5eeeb61d138d45d4f808
Robert Pearce <me@robertwpearce.com>
2020-05-03 19:46:34 -0400

    add homepage

--- derivation.nix
+++ derivation.nix
@@ -10,6 +10,7 @@ pkgs.stdenv.mkDerivation {
   '';
   meta = {
     description = "Practice flashcards in bash";
+    homepage = "https://github.com/rpearce/bashcards/";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ rpearce ];
     platforms = lib.platforms.all;
